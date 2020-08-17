e8978e936e8005878d3dcb62ca242a66802957fe
Denis Polom <dpolom@suse.de>
2020-08-17 14:01:00 +0200

    updated deployment script

--- SES7/remote-exec/master/2_deploy_ses.sh
+++ SES7/remote-exec/master/2_deploy_ses.sh
@@ -14,6 +14,7 @@ salt \* saltutil.sync_all
 sleep 15
 
 ceph-salt config /ceph_cluster/minions add "*"
+ceph-salt config /ceph_cluster/roles/cephadm add "*"
 ceph-salt config /ceph_cluster/roles/admin add "$master"
 
 ceph-salt config /ceph_cluster/roles/bootstrap set "${monitors[0]}"
@@ -26,6 +27,9 @@ done
 ceph-salt config /ssh generate
 ceph-salt config /time_server/server_hostname set "$master"
 ceph-salt config /time_server/external_servers add "ntp.suse.cz"
+ceph-salt config /cephadm_bootstrap/dashboard/username set admin
+ceph-salt config /cephadm_bootstrap/dashboard/password set admin
+ceph-salt config /cephadm_bootstrap/dashboard/force_password_update disable
 ceph-salt config /containers/registries add prefix=registry.suse.de location=172.16.0.24:5000 insecure=true
 ceph-salt config /containers/images/ceph set "registry.suse.de/suse/sle-15-sp2/update/products/ses7/milestones/containers/ses/7/ceph/ceph"
 ceph-salt config ls
@@ -57,7 +61,7 @@ ceph orch apply -i cluster.yaml
 # wait until all OSDs are deployed
 for i in ${osd_nodes[@]%%.*}
 do  
-    while [ ! "$(ceph osd tree --format=json | jq -r '.nodes[] | .name, .status'  | grep -v default | sed 's/null//g' | tr '\n' ' ' | awk "/$i/ && /osd./ && ! /down/{print \$0}")" ]
+    while [ ! "$(ceph osd tree --format=json | jq -r '.nodes[] | .name, .status'  | grep -v default | sed 's/null//g' | tr '\n' ' ' | awk "/$i/ && /osd./ && ! /down/{print \$0}")" ] || [  "$(ceph osd tree --format=json | jq -r '.stray[] | .status' | grep down)" ]
     do
         sleep 60
     done
@@ -66,3 +70,8 @@ done
 
 ceph -s
 
+ceph mgr module disable dashboard
+
+while [ "$(ceph mgr services --format json | jq -r .dashboard)" != "null" ];do 
+    sleep 5
+done
