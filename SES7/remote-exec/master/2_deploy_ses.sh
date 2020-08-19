d92cad65367f3e3d01062c82a48240a871459034
Denis Polom <dpolom@suse.de>
2020-08-19 13:28:29 +0200

    removed cephadm for all and kept only for monitors

--- SES7/remote-exec/master/2_deploy_ses.sh
+++ SES7/remote-exec/master/2_deploy_ses.sh
@@ -14,7 +14,7 @@ salt \* saltutil.sync_all
 sleep 15
 
 ceph-salt config /ceph_cluster/minions add "*"
-ceph-salt config /ceph_cluster/roles/cephadm add "*"
+ceph-salt config /ceph_cluster/roles/cephadm add "$master"
 ceph-salt config /ceph_cluster/roles/admin add "$master"
 
 ceph-salt config /ceph_cluster/roles/bootstrap set "${monitors[0]}"
@@ -22,6 +22,7 @@ ceph-salt config /ceph_cluster/roles/bootstrap set "${monitors[0]}"
 for i in ${monitors[@]}
 do
     ceph-salt config /ceph_cluster/roles/admin add "$i"
+    ceph-salt config /ceph_cluster/roles/cephadm add "$i"
 done
 
 ceph-salt config /ssh generate
@@ -30,7 +31,7 @@ ceph-salt config /time_server/external_servers add "ntp.suse.cz"
 ceph-salt config /cephadm_bootstrap/dashboard/username set admin
 ceph-salt config /cephadm_bootstrap/dashboard/password set admin
 ceph-salt config /cephadm_bootstrap/dashboard/force_password_update disable
-ceph-salt config /containers/registries add prefix=registry.suse.de location=172.16.0.24:5000 insecure=true
+ceph-salt config /containers/registries_conf/registries add prefix=registry.suse.de location=172.16.0.24:5000 insecure=true
 ceph-salt config /containers/images/ceph set "registry.suse.de/suse/sle-15-sp2/update/products/ses7/milestones/containers/ses/7/ceph/ceph"
 ceph-salt config ls
 
