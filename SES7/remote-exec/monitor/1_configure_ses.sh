f95c9f0a7a747dcd5fe9a93494b6b18c3b5b60d9
Denis Polom <dpolom@suse.de>
2020-08-25 15:58:31 +0200

    cluster network for osd only

--- SES7/remote-exec/monitor/1_configure_ses.sh
+++ SES7/remote-exec/monitor/1_configure_ses.sh
@@ -10,8 +10,8 @@ ceph config set global mon_clock_drift_allowed 2.0
 
 ceph config set osd debug_ms 1
 
-ceph config set global cluster_network 172.16.0.0/23
+ceph config set mon cluster_network 172.16.0.0/23
 
-ceph config set global public_network 172.18.0.0/23
+ceph config set osd public_network 172.18.0.0/23
 
 ceph config dump
